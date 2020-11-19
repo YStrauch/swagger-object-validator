26b57c2adafe535b73c0ac6b9bba2436aebb194e
Yaron Strauch <git@yaron-strauch.com>
2020-11-19 20:13:21 +0000

    test: #10 - add test case from report

--- test/handler.spec.ts
+++ test/handler.spec.ts
@@ -37,6 +37,50 @@ describe('Handler', () => {
     }).catch(err => done(new Error(err)));
   });
 
+  it('should not crash when allowing null', (done) => {
+    let validator = new Handler('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/yaml/petstore.yaml', {
+        allowAdditionalProperties: true,
+        ignoreError: (error, value, schema, spec) => {
+          // Set Ignore cases where non-required fields are set to null rather than specified type/format
+          return error.errorType === ValidationErrorType.TYPE_MISMATCH && value === null;
+        }
+      });
+
+    var pet = {
+      id: 3942394023,
+      name: 'Pippin',
+      tag: <any> null
+    }
+
+    validator.validateModel(pet, 'Pet', function (err, result) {
+      expect(result.errors).to.empty;
+      done();
+    });
+
+
+    // let config: IValidatorConfig = {
+    //   partialsDir: dir,
+    //   ignoreError: (error, value, schema) => {
+    //     return error.errorType === ValidationErrorType.TYPE_MISMATCH
+    //       && error.trace[0].stepName === 'Pet'
+    //       && error.trace[1].stepName === 'name'
+    //       && value === null;
+    //   }
+    // };
+
+    // let validator = new Handler(yaml, config);
+
+    // let pet: any = {
+    //   id: 123,
+    //   name: null
+    // };
+    // validator.validateModel(pet, 'Pet').then(result => {
+    //   expect(result.errors).to.empty;
+
+    //   done();
+    // }).catch(err => done(new Error(err)));
+  });
+
   it('should allow custom validation synchronously', (done) => {
     let config: IValidatorConfig = {
       partialsDir: dir,
